e9b5e1c27731e2df261110a60207477afb2c9be7
Ilya Safonov <safonovil17@yandex.ru>
2021-12-13 20:37:48 +0300

    create: настроил мобильное меню

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  document.querySelector(".header__navbar").classList.toggle("header__navbar-visible");
+});
